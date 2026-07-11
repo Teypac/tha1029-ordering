56026575b87c61d2eb5cc82b40af421b444b2de5
NODADACTIVE <iteyonb@gmail.com>
2026-07-10 23:04:31 -0400

    Add landing route

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "landing.html"));
+});
 
 app.listen(3000, () => {
     console.log("Tha 10.29 running on port 3000");
